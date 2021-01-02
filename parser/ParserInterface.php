924fdabb5f8604056e5e825e9e43b49094391756
clvarley <conorlukevarley@gmail.com>
2021-01-02 22:55:34 +0000

    Parsers now return a route collection object

--- parser/ParserInterface.php
+++ parser/ParserInterface.php
@@ -2,6 +2,8 @@
 
 namespace Routing\Parser;
 
+use Routing\Collection\CollectionInterface;
+
 /**
  * Interface for classes that parse route files
  *
@@ -13,11 +15,9 @@ Interface ParserInterface
     /**
      * Parse the given routes file and return an array of routes
      *
-     * @throws \Routing\Exception\FileNotFoundException  File not found
-     * @throws \Routing\Exception\InvalidFileException   Invalid format
-     * @param  string $filename Path to file
-     * @return array            Routes array
+     * @param string $filename                          Path to file
+     * @return CollectionInterface                      Route collection
      */
-    public function parseFile( string $filename ) : array;
+    public function parse( string $filename ) : CollectionInterface;
 
 }
